fc41f89fc0d4d09b026b36771ba87038dbf8f596
Peter Lithammer <peter.lithammer@gmail.com>
2020-01-23 11:22:10 +0100

    journal 0.0.1

---
+++ Formula/journal.rb
@@ -0,0 +1,27 @@
+class Journal < Formula
+  desc "Simple journal tool for the command-line"
+  homepage "https://github.com/lithammer/journal"
+  url "https://github.com/lithammer/journal/archive/v0.0.1.tar.gz"
+  sha256 "e6a551f48864b26f322532bd404410f14d2275d396492c446ebd84f475e5697d"
+  head "https://github.com/lithammer/journal.git"
+
+  bottle :unneeded
+
+  depends_on "bash"
+  depends_on "bash-completion@2"
+
+  def install
+    bin.install "journal"
+
+    bash_completion.install "completions/journal.bash" => "kubectx"
+    zsh_completion.install "completions/journal.zsh" => "_kubectx"
+    fish_completion.install "completions/journal.fish"
+
+    system "make", "man"
+    man.install "man/journal.1"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/journal -h 2>&1")
+  end
+end
